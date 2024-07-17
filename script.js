829c9468925757687933c048caf1dd23db2cb17a
Your Name <charmainemsibi02@gmail.com>
2024-07-17 12:17:47 +0200

    Adding function for the hamburger

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* Funtion for hamburger targeting 2 elements*/
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("span");
+    icon.classList.toggle("span");
+    
+}
